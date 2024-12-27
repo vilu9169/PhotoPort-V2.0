db53d5a2c856cbcd35563cbbd0653e4abc39a05a
vilu9169 <viktor.lundin3@gmail.com>
2024-12-27 11:36:46 +0100

    Small fix to Contact page

--- frontend/src/app/about.js
+++ frontend/src/app/about.js
@@ -2,7 +2,7 @@
 const About = () => {
     return (
         <div>
-            <div className=" text-center sm:text-center md:text-left lg:text-left grid space-y-8 grid-cols-1 mt-8  justify-items-center">
+            <div className=" text-center sm:text-center md:text-left lg:text-left grid space-y-8 grid-cols-1 mt-8  justify-items-center pb-8">
             
                 <img src="/ViktorLundin.jpg" alt="Viktor Lundin" className=" relative rounded-full w-40 h-40 align-middle" />
                 <div className=" relative align-middle w-1/2">
