66b2a81f2ca033c109418032f8b82de785c83dcf
vilu9169 <viktor.lundin3@gmail.com>
2024-12-28 16:58:28 +0100

    Fixed so you can click photos to see

--- frontend/src/app/gallery.js
+++ frontend/src/app/gallery.js
@@ -1,11 +1,20 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Gallery = () => {
   const [images, setImages] = useState([]);
   const [aspectRatios, setAspectRatios] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
 
+
+
+  const pageVariants = {
+    initial: { opacity: 0, x: 50 },
+    animate: { opacity: 1, x: 0 },
+    exit: { opacity: 0, x: -50 },
+  };
   useEffect(() => {
     // Fetch images from API
     fetch("http://127.0.0.1:8000/api/photos/")
@@ -41,38 +50,100 @@ const Gallery = () => {
     }
   }, [images]);
 
+  const handlePhotoClick = (photo) => {
+    setSelectedPhoto(photo);
+  };
+
+  const handleBackToGallery = () => {
+    setSelectedPhoto(null);
+  };
+
   return (
     <div className="justify-items-center mt-4">
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
+      <AnimatePresence>
+        {/* Detail View */}
+        {selectedPhoto && (
+          <motion.div
+            key="photo-detail"
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.8 }}
+            transition={{ duration: 0.25 }}
+            className=" items-center justify-center z-10"
+          >
+                <button
+                onClick={handleBackToGallery}
+                className=" py-2 px-4 hover:underline underline-offset-4"
+              >
+                Back to Gallery
+              </button>
+            <div className=" mx-9 mt-5 grid grid-cols-1 md:grid-cols-2">
+              <img
+                src={`http://127.0.0.1:8000${selectedPhoto.image}`}
+                alt={selectedPhoto.title}
+                className=" max-h-[600px] mb-4"
+              />
+              <div>
+              <h2 className="text-2xl font-bold mb-2">{selectedPhoto.title}</h2>
+              <p className="text-gray-700 dark:text-gray-300 mb-4">
+                {selectedPhoto.description}
+              </p>
+              </div>
+            </div>
+            </motion.div>
+        )}
+      </AnimatePresence>
+
+      {/* Gallery View */}
+      {!selectedPhoto && (
+        <motion.div
+        key="gallery"
+        variants={pageVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ duration: 0.5, delay: 0.30   }}
+      >
         <div
           className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-20 md:mx-40 pb-11"
           style={{ gridAutoRows: "minmax(200px, auto)" }}
         >
-          {images.map((image, index) => {
-            const aspectRatio = aspectRatios[index];
-            const isPortrait = aspectRatio < 1;
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            images.map((image, index) => {
+              const aspectRatio = aspectRatios[index];
+              const isPortrait = aspectRatio < 1;
 
-            return (
-              <div
-                key={image.id}
-                className={`relative bg-gray-200 dark:bg-gray-800 overflow-hidden shadow-lg transition-all duration-300 ease-in-out ${
-                  isPortrait ? "col-span-1 row-span-2" : "col-span-1 row-span-1"
-                }`}
-              >
-                <img
-                  src={`http://127.0.0.1:8000${image.image}`}
-                  alt={image.title}
-                  className="w-full object-cover h-full transition-transform duration-300 ease-in-out"
-                />
-              </div>
-            );
-          })}
+              return (
+                <motion.div
+                  key={image.id}
+                  layout
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  whileHover={{ scale: 1.05 }}
+                  onClick={() => handlePhotoClick(image)}
+                  className={`relative bg-gray-200 dark:bg-gray-800 overflow-hidden shadow-lg transition-all duration-300 ease-in-out cursor-pointer ${
+                    isPortrait ? "col-span-1 row-span-2" : "col-span-1 row-span-1"
+                  }`}
+                >
+                  <img
+                    src={`http://127.0.0.1:8000${image.image}`}
+                    alt={image.title}
+                    className="w-full object-cover h-full transition-transform duration-300 ease-in-out"
+                  />
+                </motion.div>
+              );
+            })
+          )}
         </div>
+        </motion.div>
       )}
     </div>
+
   );
+  
 };
 
 export default Gallery;
