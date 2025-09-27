2695daf5ed6d870a371b967509952770f0112813
vilu9169 <viktor.lundin3@gmail.com>
2025-09-27 21:45:46 +0200

    fix access, and clean up objects

--- backend/portfolio/urls.py
+++ backend/portfolio/urls.py
@@ -2,15 +2,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from . import views
+from django.contrib.admin.views.decorators import staff_member_required
 
 urlpatterns = [
-    path('', views.photo_list, name='photo_list'),
-    path('upload/', views.upload_photo, name='upload_photo'),
-    path('api/photos/', views.PhotoList.as_view(), name='photo_list_api'),
-    path('up_order/<int:id>/', views.up_order, name='up'),
-    path('down_order/<int:id>/', views.down_order, name='down'),
-    path('top_order/<int:id>/', views.top_order, name='top'),
-    path('bottom_order/<int:id>/', views.bottom_order, name='bottom'),
+    path('', staff_member_required(views.photo_list), name='photo_list'),
+
+    path('upload/', staff_member_required(views.upload_photo), name='upload_photo'),
+    path('up_order/<int:id>/', staff_member_required(views.up_order), name='up'),
+    path('down_order/<int:id>/', staff_member_required(views.down_order), name='down'),
+    path('top_order/<int:id>/', staff_member_required(views.top_order), name='top'),
+    path('bottom_order/<int:id>/', staff_member_required(views.bottom_order), name='bottom'),
+
+    path('api/photos/', staff_member_required(views.PhotoList.as_view()), name='photo_list_api'),
 ]
 
 # Only serve local files if DEBUG=True
