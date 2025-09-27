6ebef747d4853e9615d1abc056dacd27570454b0
vilu9169 <viktor.lundin3@gmail.com>
2025-09-28 00:11:41 +0200

    fixed frontend

--- backend/portfolio/serializer.py
+++ backend/portfolio/serializer.py
@@ -6,3 +6,6 @@ class PhotoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Photo
         fields = ['id', 'title', 'description', 'image', 'created_at']
+
+    def get_image_url(self, obj):
+        return obj.image.url if obj.image else None
