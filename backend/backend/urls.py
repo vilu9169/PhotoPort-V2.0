31c758c0987ac1baceddb27931ce8f23c65e53b3
vilu9169 <viktor.lundin3@gmail.com>
2025-09-27 21:56:09 +0200

    fix urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -23,6 +23,7 @@ from django.urls import path, include
 from django.http import HttpResponse
 def health(request): return HttpResponse("ok")
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path("health/", health),
     path("", include("portfolio.urls")),
 ]
