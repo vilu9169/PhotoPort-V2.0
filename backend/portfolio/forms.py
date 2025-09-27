b01dc12f8dcf135d8a35cea4d7c6e4654e878e53
vilu9169 <viktor.lundin3@gmail.com>
2025-09-27 23:37:33 +0200

    Added folder function

--- backend/portfolio/forms.py
+++ backend/portfolio/forms.py
@@ -4,4 +4,4 @@ from .models import Photo
 class PhotoForm(forms.ModelForm):
     class Meta:
         model = Photo
-        fields = ['title', 'description', 'image']
+        fields = ['title', 'description', 'folder',  'image']
